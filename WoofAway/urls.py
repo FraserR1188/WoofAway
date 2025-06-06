dfcd078ef318265563e1015c481debf9f0255a7c
Robbie Fraser <robbie.fraser@students.codeinstitute.net>
2025-06-06 06:35:18 +0100

    Added home app and templates

--- WoofAway/urls.py
+++ WoofAway/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/accounts/login/")),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
