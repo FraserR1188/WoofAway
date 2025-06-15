70325fae6cfc4bf4d6d22ce805dab6414b6a1696
Robbie Fraser <robbie.fraser@students.codeinstitute.net>
2025-06-15 11:10:07 +0100

    Fixed static issue

--- WoofAway/settings.py
+++ WoofAway/settings.py
@@ -179,6 +179,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
